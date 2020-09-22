c082934742b72ce085da339ada4591080776066f
Arda Karaman <ardanincimaili@gmail.com>
2020-09-22 14:53:30 +0300

    initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D player;
+
+    private void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
